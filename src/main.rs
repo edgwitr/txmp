5067f226e1a96209c7dcc9b7ad3788926fb7583e
EDOGAWA iteru <EdgarAether@outlook.jp>
2024-11-03 17:26:40 +0900

    fix ctrl+c

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,34 @@
 use std::env;
 use std::io;
 use std::process::{Command, Stdio};
+use std::sync::{Arc, Mutex};
+use ctrlc;
+use dotenvy::dotenv;
 
 fn main() -> io::Result<()> {
-    // 現在のプラットフォームを確認し、対応するシェルを設定
-    let shell = if cfg!(target_os = "windows") {
-        "cmd"
-    } else {
-        &env::var("SHELL").unwrap_or_else(|_| String::from("/bin/sh"))
-    };
+    dotenv().ok();
+    let shell = env::var("SHELL").unwrap_or_else(|_| {
+        if cfg!(target_os = "windows") {
+            String::from("C:\\Windows\\system32\\cmd.exe")
+        } else {
+            String::from("/bin/sh")
+        }
+    });
 
-    let mut child = Command::new(shell)
-        .stdin(Stdio::inherit())
-        .stdout(Stdio::inherit())
-        .stderr(Stdio::inherit())
-        .spawn()?; // 子プロセスとして仮想端末を起動
+    let child = Arc::new(Mutex::new(
+        Command::new(shell)
+            .stdin(Stdio::inherit())
+            .stdout(Stdio::inherit())
+            .stderr(Stdio::inherit())
+            .spawn()?,
+    ));
 
-    // プロセスが終了するまで待機
-    child.wait()?;
+    let child_clone = Arc::clone(&child);
+    ctrlc::set_handler(move || {
+        // Ctrl+C を受け取ったときに子プロセスにシグナルを送る
+        let _ = child_clone.lock().unwrap().kill();
+    }).expect("Error setting Ctrl-C handler");
+
+    child.lock().unwrap().wait()?;
     Ok(())
 }
